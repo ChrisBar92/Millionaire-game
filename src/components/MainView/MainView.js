c5666e2713dd786a0fca2cda958dd2e6bd857528
Krzysztof Bartkiewicz <krzysiekbar1@gmail.com>
2023-04-20 23:36:48 +0200

    basic version app is working

---
+++ src/components/MainView/MainView.js
@@ -0,0 +1,126 @@
+// import './mainView.scss'
+// import { useEffect, useMemo, useState } from 'react'
+// import Quiz from '../Quiz/Quiz'
+
+// const MainView = () => {
+// 	const [username, setUsername] = useState(null)
+// 	const [questionNumber, setQuestionNumber] = useState(1)
+// 	const [earned, setEarned] = useState('$ 0')
+
+// 	const data = [
+// 		{
+// 			id: 1,
+// 			question: 'Rolex is a company that specializes in what type of product?',
+// 			answers: [
+// 				{
+// 					text: 'Phone',
+// 					correct: false,
+// 				},
+// 				{
+// 					text: 'Watches',
+// 					correct: true,
+// 				},
+// 				{
+// 					text: 'Food',
+// 					correct: false,
+// 				},
+// 				{
+// 					text: 'Cosmetic',
+// 					correct: false,
+// 				},
+// 			],
+// 		},
+// 		{
+// 			id: 2,
+// 			question: 'When did the website `Facebook` launch?',
+// 			answers: [
+// 				{
+// 					text: '2004',
+// 					correct: true,
+// 				},
+// 				{
+// 					text: '2005',
+// 					correct: false,
+// 				},
+// 				{
+// 					text: '2006',
+// 					correct: false,
+// 				},
+// 				{
+// 					text: '2007',
+// 					correct: false,
+// 				},
+// 			],
+// 		},
+// 		{
+// 			id: 3,
+// 			question: 'Who played the character of harry potter in movie?',
+// 			answers: [
+// 				{
+// 					text: 'Johnny Deep',
+// 					correct: false,
+// 				},
+// 				{
+// 					text: 'Leonardo Di Caprio',
+// 					correct: false,
+// 				},
+// 				{
+// 					text: 'Denzel Washington',
+// 					correct: false,
+// 				},
+// 				{
+// 					text: 'Daniel Red Cliff',
+// 					correct: true,
+// 				},
+// 			],
+// 		},
+// 	]
+
+// 	const moneyPyramid = useMemo(
+// 		() =>
+// 			[
+// 				{ id: 1, amount: '$ 100' },
+// 				{ id: 2, amount: '$ 200' },
+// 				{ id: 3, amount: '$ 300' },
+// 				{ id: 4, amount: '$ 500' },
+// 				{ id: 5, amount: '$ 1000' },
+// 				{ id: 6, amount: '$ 2000' },
+// 				{ id: 7, amount: '$ 4000' },
+// 				{ id: 8, amount: '$ 8000' },
+// 				{ id: 9, amount: '$ 16000' },
+// 				{ id: 10, amount: '$ 32000' },
+// 				{ id: 11, amount: '$ 64000' },
+// 				{ id: 12, amount: '$ 125000' },
+// 				{ id: 13, amount: '$ 250000' },
+// 				{ id: 14, amount: '$ 500000' },
+// 				{ id: 15, amount: '$ 1000000' },
+// 			].reverse(),
+// 		[]
+// 	)
+
+// 	useEffect(() => {
+// 		questionNumber > 1 && setEarned(moneyPyramid.find(m => m.id === questionNumber - 1).amount)
+// 	}, [questionNumber, moneyPyramid])
+
+// 	return (
+// 		<>
+// 			<div className="main">
+// 				<div className="main__quiz">
+// 					<Quiz data={data} questionNumber={questionNumber} setQuestionNumber={setQuestionNumber} />
+// 				</div>
+// 			</div>
+// 			<div className="money">
+// 				<ul className="money__list">
+// 					{moneyPyramid.map(step => (
+// 						<li key={step.id} className={questionNumber === step.id ? 'money__list-item active' : 'money__list-item'}>
+// 							<span className="money__list-item--number">{step.id}</span>
+// 							<span className="money__list-item--amount">{step.amount}</span>
+// 						</li>
+// 					))}
+// 				</ul>
+// 			</div>
+// 		</>
+// 	)
+// }
+
+// export default MainView
